558f529ea92041dc048f94b945ff03ccc4774d11
陈家辉 <chenjiahui@haolianluo.com>
2017-07-17 15:00:31 +0800

    将获取地理位置的方法参数改成属性

--- src/Geolocation/BaiduGeolocation.php
+++ src/Geolocation/BaiduGeolocation.php
@@ -4,11 +4,18 @@ use graychen\Contracts\geolocationInterface;
 
 class BaiduGeolocation implements geolocationInterface
 {
+    public $latitude;
+
+    public $longitude;
+
+    public $ak;
 
     public function getGeolocation($latitude,$longitude){
-        $url="http://api.map.baidu.com/geocoder/v2/?location=".$latitude.",".$longitude."&output=json&pois=1&ak=W21wGbOjjGvNAARxxkyWcNHWzVFNITX0";
+        $url="http://api.map.baidu.com/geocoder/v2/?location=".$this->latitude.",".$this->longitude."&output=json&pois=1&ak=".$this->ak;
         $file_contents=file_get_contents($url);
         $json_content=json_decode($file_contents, true);
         return $json_content;
     }
+
+
 }
